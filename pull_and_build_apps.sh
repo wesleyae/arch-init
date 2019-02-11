5e54d412edd2c599314ea863b15f3045164a1216
Wesley Eledui <wesley.eledui@hexagonsi.com>
2019-02-11 16:41:59 -0600

    cleanup script a bit

--- pull_and_build_apps.sh
+++ pull_and_build_apps.sh
@@ -4,16 +4,22 @@ TOOLS_DIR=~/.tools
 
 mkdir "${TOOLS_DIR}"
 
+pushd "${TOOLS_DIR}"
 
 # dwm
 git clone https://git.suckless.org/dwm "${TOOLS_DIR}/dwm"
+cd "${TOOLS_DIR}/dwm"
 sudo make clean install
 
 # st
 git clone https://git.suckless.org/st "${TOOLS_DIR}/st"
 cp st/config.h "${TOOLS_DIR}/st" 
+cd "${TOOLS_DIR}/st"
 sudo make clean install
 
 # surf
-git clone https://git.suckless.org/surf "${TOOOLS_DIR}/surf"
+git clone https://git.suckless.org/surf "${TOOLS_DIR}/surf"
+cd "${TOOLS_DIR}/surf" 
 sudo make clean install
+
+popd
