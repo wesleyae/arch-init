fa4a30676ccd2da52611741669795c2cb9c5aa87
Wesley <someone@email.com>
2019-02-11 20:18:02 -0600

    fix some issues

--- pull_and_build_apps.sh
+++ pull_and_build_apps.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
 TOOLS_DIR=~/.tools
-
+CUSTOM_DIR="$(pwd)"
 mkdir "${TOOLS_DIR}"
 
-pushd "${TOOLS_DIR}"
-
 # dwm
-git clone https://git.suckless.org/dwm "${TOOLS_DIR}/dwm"
-cd "${TOOLS_DIR}/dwm"
-sudo make clean install
+if [ -d "${TOOLS_DIR}/dwm" ]
+then
+	cd "${TOOLS_DIR}/dwm"
+	git pull
+else
+	git clone https://git.suckless.org/dwm "${TOOLS_DIR}/dwm"
+	sudo make clean install
+fi
+
 
 # st
-git clone https://git.suckless.org/st "${TOOLS_DIR}/st"
-cp st/config.h "${TOOLS_DIR}/st" 
+if [ -d "${TOOLS_DIR}/st" ]
+then
+	cd "${TOOLS_DIR}/st"
+	git pull
+	rm "${TOOLS_DIR}/st/config.h"
+else
+	git clone https://git.suckless.org/st "${TOOLS_DIR}/st"
+fi
+cp "${CUSTOM_DIR}/st/config.h" "${TOOLS_DIR}/st/config.h"
 cd "${TOOLS_DIR}/st"
 sudo make clean install
 
 # surf
-git clone https://git.suckless.org/surf "${TOOLS_DIR}/surf"
+if [ -d "${TOOLS_DIR}/surf" ]
+then
+	cd "${TOOLS_DIR}/surf"
+	git pull
+else
+	git clone https://git.suckless.org/surf "${TOOLS_DIR}/surf"
+fi
 cd "${TOOLS_DIR}/surf" 
 sudo make clean install
 
-popd
-
-gcc -o barM barM.c -O2 -s -lX11
-sudo cp barM /usr/local/bin/
-
+# barM
+gcc -o "${CUSTOM_DIR}/barM" "${CUSTOM_DIR}/barM.c" -O2 -s -lX11
+sudo cp "${CUSTOM_DIR}/barM" /usr/local/bin/
